e13ce1204229c201c73d21d023f253cce08f7c2f
myung4386 <myung4386@naver.com>
2021-05-01 15:50:24 +0900

    fix NaN error

--- baseball_data_crawling/Team_data_extraction_RunningBanana/hitter_processing/hitter_processing.py
+++ baseball_data_crawling/Team_data_extraction_RunningBanana/hitter_processing/hitter_processing.py
@@ -12,13 +12,15 @@ for i in range(2017, 2021):
     table['타율'] = table['안타'] / table['타수'] # 타율: 안타/타수
     table['타수대비홈런'] = table['홈런'] / table['타수'] # 타수대비홈런: 홈런/타수
     table['타수대비삼진'] = table['삼진'] / table['타수'] # 타수대비삼진: 삼진/타수
-    table['타수대비사구'] = table['타수'] / table['사구'] # 타수대비사구: 타수/사구
-    table['타수대비득점'] = table['타수'] / table['득점'] # 타수대비득점: 타수/득점
-    table['타수대비타점'] = table['타수'] / table['타점'] # 타수대비타점: 타수/타점
+    table['타수대비사구'] = table['사구'] / table['타수'] # 타수대비사구: 사구/타수
+    table['타수대비득점'] = table['득점'] / table['타수'] # 타수대비득점: 득점/타수
+    table['타수대비타점'] = table['타점'] / table['타수'] # 타수대비타점: 타점/타수
     table['출루율'] = (table['사구'] + table['안타']) / (table['타수'] + table['사구'] + table['희타']) # 출루율 : (사구+안타)/(타수+사구+희타)
     table['도루성공률'] = table['도루'] / (table['도루'] + table['도실']) # 도루성공률 : 도루성공/(도루성공 + 도루실패)
     ordinary_columns.remove('이름')
     table.drop(ordinary_columns, axis=1, inplace=True)
+
+    table = table.fillna(0)
 
     print(table)
     print(table.columns)
